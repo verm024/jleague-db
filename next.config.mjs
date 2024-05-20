0f3a5e2a5a2692cfceb4642d5003924daa0ddc8f
Muhammad Naufal <muhnaufal251@gmail.com>
2024-05-20 23:10:29 +0700

    chore: initial setup gh pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ["media.api-sports.io"],
+    domains: ['media.api-sports.io'],
   },
 };
 
